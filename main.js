cbcade1604367cd52733d83e0ff967d8f23e1a81
Arjun Shah <arjun@nbp-213-78.nbp.ruw.rutgers.edu>
2022-02-02 06:33:11 -0500

    Initial commit

--- main.js
+++ main.js
@@ -9,3 +9,7 @@ const mobileMenuBtn = document.getElementById('mobile-menu-cta')
                   nav.classList.remove('menu-btn');
               })
             
+/* will be adding the actual JS functionality of populating a list of dog breeds
+or predicting search results as the user types a dog breed. The first one would
+be easier but the second one would look cooler. Then making API calls and displaying 
+an image and eventually info of the dog breed */ 
